d14f9f75c5e1553ecebb421271ba64dd36cae5c6
Steve Zhao <ampersand38@gmail.com>
2021-02-12 15:07:43 -0500

    Add opfor helis for Support mission (#39)
    * Add opfor helis for Support mission
    
    https://youtu.be/_eGtIla8TZk
    
    * add mission
    
    * mission packaged

---
+++ addons/uh60_opfor/config.cpp
@@ -0,0 +1,26 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+    		"vtx_UH60M_O",
+    		"vtx_HH60_O",
+            "vtx_MH60M_O",
+            "vtx_MH60M_DAP_O",
+            "vtx_MH60S_Pylons_GAU21L_O",
+            "vtx_MH60S_GAU21L_O",
+            "vtx_MH60S_Pylons_O",
+            "vtx_MH60S_O"
+        };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"vtx_UH60", "vtx_MH60M", "vtx_MH60S"};
+        author = "";
+        authors[] = {""};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "cfgVehicles.hpp"
