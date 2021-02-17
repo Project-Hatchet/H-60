e45e4a46ad5aeb8f037791977f49022102703e85
Steve Zhao <ampersand38@gmail.com>
2021-02-17 01:22:33 -0500

    ViV and DAP seat hide

--- addons/MH60M/config/cfgVehicles.hpp
+++ addons/MH60M/config/cfgVehicles.hpp
@@ -122,6 +122,21 @@ class CfgVehicles {
             #include "cargoTurrets.hpp"
         };
         hiddenSelectionsTextures[] = {"","","","","","","","","","","","","","","","","z\vtx\addons\MH60M\data\main_co.paa","z\vtx\addons\MH60M\Data\Misc_co.paa","z\vtx\addons\MH60M\data\tail_co.paa","","","","","","z\vtx\addons\UH60\data\FuelProbe\Fuel_probe_co.paa"};
+        class VehicleTransport {
+        	class Carrier {
+        		cargoBayDimensions[]		= {"VTV_limit_1", "VTV_limit_2"};	// Memory points in model defining cargo space
+        		disableHeightLimit			= 0;								// If set to 1 disable height limit of transported vehicles
+        		maxLoadMass					= 4100;							// Maximum cargo weight (in Kg) which the vehicle can transport
+        		cargoAlignment[]			= {"front", "center"};				// Array of 2 elements defining alignment of vehicles in cargo space. Possible values are left, right, center, front, back. Order is important.
+        		cargoSpacing[]				= {0.3, 0.3, 0};						// Offset from X,Y,Z axes (in metres)
+        		exits[]						= {"pos Cargo L", "pos Cargo R"};		// Memory points in model defining loading ramps, could have multiple
+        		unloadingInterval			= 2;								// Time between unloading vehicles (in seconds)
+        		loadingDistance				= 5;								// Maximal distance for loading in exit point (in meters).
+        		loadingAngle				= 60;								// Maximal sector where cargo vehicle must be to for loading (in degrees).
+        		parachuteClassDefault       = B_Parachute_02_F;					// Type of parachute used when dropped in air. Can be overridden by parachuteClass in Cargo.
+        		parachuteHeightLimitDefault = 50;								// Minimal height above terrain when parachute is used. Can be overriden by parachuteHeightLimit in Cargo.
+        	};
+        };
     }; // vtx_MH60M
     class vtx_MH60M_DAP : vtx_MH60M {
         scope = 2;
@@ -194,7 +209,11 @@ class CfgVehicles {
                 animPeriod=1;
                 initPhase=1;
             };
-
+            class GunnerSeats_Hide {
+                source="user";
+                animPeriod=1;
+                initPhase=1;
+            };
             class CabinSeats_Hide {
                 source="user";
                 animPeriod=1;
@@ -221,6 +240,7 @@ class CfgVehicles {
         gunBeg[] = {"muzzle_1","muzzle_2"};
         gunEnd[] = {"chamber_1","chamber_2"};
         selectionFireAnim = "zasleh_12";
+        class VehicleTransport {};
     }; // vtx_H60_base
 }; // CfgVehicles
 
