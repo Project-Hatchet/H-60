d6cbb9b27e50d4f7517268e7490c811f82a098f1
Steve Zhao <ampersand38@gmail.com>
2021-02-22 01:45:51 -0500

    base model no missile cam

--- addons/UH60/config/cfgVehiclesParts/components.hpp
+++ addons/UH60/config/cfgVehiclesParts/components.hpp
@@ -5,31 +5,4 @@ class Components: Components {
 			class DatalinkSensorComponent: SensorTemplateDataLink {};
 		};
 	};
-
-    class VehicleSystemsDisplayManagerComponentLeft: VehicleSystemsTemplateLeftPilot
-    {
-    	class Components: Components
-    	{
-    		class MissileDisplay	// Camera feed from missile's warhead
-    		{
-    			componentType = "TransportFeedDisplayComponent";
-    			source = "Missile";
-    			// resource = "RscTransportCameraComponentMissile"; // hardcoded
-    		};
-    	};
-    }; // vtx_VehicleSystemsDisplayManagerLeft
-
-    class VehicleSystemsDisplayManagerComponentRight : VehicleSystemsTemplateRightPilot
-    {
-    	class Components: Components
-    	{
-    		class MissileDisplay	// Camera feed from missile's warhead
-    		{
-    			componentType = "TransportFeedDisplayComponent";
-    			source = "Missile";
-    			// resource = "RscTransportCameraComponentMissile"; // hardcoded
-    		};
-    	};
-    }; //  vtx_VehicleSystemsDisplayManagerRight
-
 };
