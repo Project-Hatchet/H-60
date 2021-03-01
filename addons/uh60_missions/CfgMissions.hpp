18824cc0ff0500b543af9e57b92646265e95be57
Steve Zhao <ampersand38@gmail.com>
2021-03-01 00:01:27 -0500

    missing class MPMissions

--- addons/uh60_missions/CfgMissions.hpp
+++ addons/uh60_missions/CfgMissions.hpp
@@ -1,9 +1,11 @@
-class MPMissions {
-    class VTX_GroundSupport01 {
-        briefingName = "CO20 H-60 Support Rodopoli";
-        author = "Ampersand";
-        overviewText = "$STR_A3_MP_GS_SC_01_Overview_text";
-        overview = "";
-        directory = "z\vtx\addons\uh60_missions\VTX_GroundSupport01.Altis";
-    };
+class CfgMissions {
+  class MPMissions {
+      class VTX_GroundSupport01 {
+          briefingName = "CO20 H-60 Support Rodopoli";
+          author = "Ampersand";
+          overviewText = "$STR_A3_MP_GS_SC_01_Overview_text";
+          overview = "";
+          directory = "z\vtx\addons\uh60_missions\VTX_GroundSupport01.Altis";
+      };
+  };
 };
