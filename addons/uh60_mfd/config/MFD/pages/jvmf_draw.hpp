f45319dacb62261e19e668707575014feaa89577
YannikH <yannikhegge@gmail.com>
2021-11-15 19:11:19 +0100

    Possible JVMF page redesign

--- addons/uh60_mfd/config/MFD/pages/jvmf_draw.hpp
+++ addons/uh60_mfd/config/MFD/pages/jvmf_draw.hpp
@@ -13,28 +13,28 @@ class backgroundWrapper {
 	};
 }; // backgroundWrapper
 
-#define JVMF_LINE_HEIGHT 0.06
+#define JVMF_LINE_HEIGHT 0.08
 #define JVMF_LEFT_ALIGN 0.1
 #define JVMF_TOP_MARGIN 0.16
 
-TEXT_LEFT_MID_USERTEXT(JVMF_MESSAGE_INBOX,0.01,0.38,42)
+TEXT_LEFT_SCALED_SRC(JVMF_MESSAGE_INBOX,0.01,0.38,"PAGE",0.05)
 
-TEXT_MID_MID(JVMF_REPLY_ACK,0.14,JVMF_LINE_HEIGHT * 0.2,"ACK")
+TEXT_MID_SCALED(JVMF_REPLY_ACK,0.35,JVMF_LINE_HEIGHT * 0.3,"ACK",0.04)
 class wilco_wrap {
     color[] = common_yellow;
-    TEXT_MID_MID(JVMF_REPLY_WILCO,0.24,JVMF_LINE_HEIGHT * 0.2,"WILCO")
+    TEXT_MID_SCALED(JVMF_REPLY_WILCO,0.64,JVMF_LINE_HEIGHT * 0.3,"WILCO",0.04)
 };
 class haveco_wrap {
     color[] = common_green;
-    TEXT_MID_MID(JVMF_REPLY_HAVECO,0.74,JVMF_LINE_HEIGHT * 0.2,"HAVECO")
+    TEXT_MID_SCALED(JVMF_REPLY_HAVECO,0.74,JVMF_LINE_HEIGHT * 0.3,"HAVECO",0.04)
 };
 class cantco_wrap {
     color[] = common_red;
-    TEXT_MID_MID(JVMF_REPLY_CANTCO,0.86,JVMF_LINE_HEIGHT * 0.2,"CANTCO")
+    TEXT_MID_SCALED(JVMF_REPLY_CANTCO,0.86,JVMF_LINE_HEIGHT * 0.3,"CANTCO",0.04)
 };
 class reply_options_blue {
     color[] = common_blue;
-    TEXT_MID_MID(JVMF_REPLY_OPTIONS,0.5,JVMF_LINE_HEIGHT * 0.5,"REPLY OPTIONS")
+    TEXT_MID_SCALED(JVMF_REPLY_OPTIONS,0.5,JVMF_LINE_HEIGHT * 0.5,"",0.04)
 };
 
 #define JVMF_BORDER_MARGIN 0.02
@@ -50,18 +50,27 @@ class reply_options_blue {
     };
 
 class jvmf_type_0 {
-	condition=COND_ISNUMBER(JVMF_TYPE,0);
+	// condition=COND_ISNUMBER(JVMF_TYPE,0);
+	condition="0";
 	#include "jvmf\jvmf_0.hpp"
 };
 
 class jvmf_type_1 {
-	condition=COND_ISNUMBER(JVMF_TYPE,1);
+	// condition=COND_ISNUMBER(JVMF_TYPE,1);
+	condition="0";
 	#include "jvmf\jvmf_1.hpp"
 };
 
 class jvmf_type_2 {
-	condition=COND_ISNUMBER(JVMF_TYPE,2);
+	// condition=COND_ISNUMBER(JVMF_TYPE,2);
+	condition="0";
 	#include "jvmf\jvmf_2.hpp"
+};
+
+class jvmf_text {
+	// condition=COND_ISNUMBER(JVMF_TYPE,0);
+	condition="1";
+	#include "jvmf\jvmf_shared.hpp"
 };
 
 #define BOTTOM_TEXT_Y 0.96
