0b7925615d8220346fe55bdf00e41ee52afc0f32
YannikH <yannikhegge@gmail.com>
2021-11-16 18:23:49 +0100

    Fixed build issues

--- addons/uh60_mfd/config/MFD/pages/tac_cmws.hpp
+++ addons/uh60_mfd/config/MFD/pages/tac_cmws.hpp
@@ -20,144 +20,146 @@ class Bones {
 };
 
 class Draw {
-        condition = COND_ISNUMBER(MFD_PAGE_INDEX,MFD_PAGE_TAC);
-        class TAC_CMWS
-        {
-            type = sensor;
+    condition = COND_ISNUMBER(MFD_PAGE_INDEX,MFD_PAGE_TAC);
+    class TAC_CMWS
+    {
+        //  type = sensor;
+        #ifdef SENSOR_SIZE
             #undef SENSOR_SIZE
-            #define SENSOR_SIZE(numb)\
-            pos[]	= {TAC_CMWS_Offset,{(0+numb)*0.725, 0+numb}, 1};\
-            down[]	= {TAC_CMWS_Offset,{(0-numb)*0.725, 0-numb}, 1};
-            SENSOR_SIZE(-0.17)
+        #endif
+        #define SENSOR_SIZE(SIZE) \
+        pos[]	= {TAC_CMWS_Offset,{(0+SIZE)*0.725, 0+SIZE}, 1}; \
+        down[]	= {TAC_CMWS_Offset,{(0-SIZE)*0.725, 0-SIZE}, 1}
+        SENSOR_SIZE(-0.17);
 
-            //showTargetTypes = 1+2+8+64+1024;    // RWR only
-            showTargetTypes = 1+2+4+8+16+32+64+128+256+512+1024;
-            width = 1; // default width of lines can by different in case of class XXXX used instead of arrays
-            range=4000;
-            sensorLineType = 2; // same as "lineType"
-            sensorLineWidth = 3;
-            class MissileThreat
+        showTargetTypes = 1+2+8+64+1024;    // RWR only
+        //showTargetTypes = 1+2+4+8+16+32+64+128+256+512+1024;
+        width = 1; // default width of lines can by different in case of class XXXX used instead of arrays
+        range=4000;
+        sensorLineType = 2; // same as "lineType"
+        sensorLineWidth = 3;
+        class MissileThreat
+        {
+            color[] = {1,0,0};
+            class MissleText{
+                type="text";
+                source="static";
+                text="M";
+                scale=1;
+                sourceScale=1;
+                align = "center";
+                pos[]= {{0.00, -0.015}, 1};
+                right[]= {{0.06, -0.015}, 1};
+                down[]= {{0.00, 0.015}, 1};
+            };
+            class MissileBox
             {
-                color[] = {1,0,0};
-                class MissleText{
-                    type="text";
-                    source="static";
-                    text="M";
-                    scale=1;
-                    sourceScale=1;
-                    align = "center";
-                    pos[]= {{0.00, -0.015}, 1};
-                    right[]= {{0.06, -0.015}, 1};
-                    down[]= {{0.00, 0.015}, 1};
+                type = "line";
+                width = 5.0000;
+                points[] ={
+                    {{-0.015, -0.015},1},
+                    {{0.015, -0.015},1},
+                    {{0.015, 0.015},1},
+                    {{-0.015, 0.015},1},
+                    {{-0.015, -0.015},1}
                 };
-                class MissileBox
-                {
-                    type = "line";
-                    width = 5.0000;
-                    points[] ={
-                        {{-0.015, -0.015},1},
-                        {{0.015, -0.015},1},
-                        {{0.015, 0.015},1},
-                        {{-0.015, 0.015},1},
-                        {{-0.015, -0.015},1}
-                    };
-                };
-            };
-            class markingThreat
-            {
-                color[] = {1,1,0}; // radar is tracking you actively
-                class background {
-                	type        = "polygon";
-                	points[] ={
-                		{
-                			{{-0.015, 0.015},1},
-                			{{0.015, 0.015},1},
-                			{{0.015, -0.015},1},
-                			{{-0.015, -0.015},1}
-                		}
-                	}; // points
-                }; // background
-                class unknownTarget
-                {
-                    type = "line";
-                    width = 5.0000;
-                    points[] ={
-                        {{-0.015, -0.015},1},
-                        {{0.015, -0.015},1},
-                        {{0.015, 0.015},1},
-                        {{-0.015, 0.015},1},
-                        {{-0.015, -0.015},1},{},
-                        {{-0.002, -0.002},1},
-                        {{0.002, 0.002},1},{},
-                        {{-0.002, 0.002},1},
-                        {{0.002, -0.002},1}
-                    };
-                };
-            };
-            class lockingThreat
-            {
-                color[] = {1,0,0};
-                class background {
-                	type        = "polygon";
-                	points[] ={
-                		{
-                			{{-0.015, 0.015},1},
-                			{{0.015, 0.015},1},
-                			{{0.015, -0.015},1},
-                			{{-0.015, -0.015},1}
-                		}
-                	}; // points
-                }; // background
-                class TargetBox
-                {
-                    type = "line"; // locking, you're in for a bad time
-                    width = 5.0000;
-                    points[] ={
-                        {{-0.015,0},1},
-                        {{0, -0.015 },1},
-                        {{0.015,0 },1},
-                        {{0, 0.015 },1},
-                        {{-0.015,0},1},{},
-                        {{-0.005,0.005},1},
-                        {{0,-0.005},1},
-                        {{0.005,0.005},1}
-                    };
-                };
-            };
-            //rwr
-            class rwr
-            {
-                class TargetBox
-                {
-                    type = "line"; // there's a radar, but stuff isn't happening yet
-                    width = 5.0000;
-                    points[] ={
-                        {{-0.015, -0.015},1},
-                        {{0.015, -0.015},1},
-                        {{0.015, 0.015},1},
-                        {{-0.015, 0.015},1},
-                        {{-0.015, -0.015},1},{},
-                        {{-0.005,0.005},1},
-                        {{0,-0.005},1},
-                        {{0.005,0.005},1}
-                    };
-                };
-            };
-            class rwrFriendly: rwr
-            {
-                color[]	= common_green;
-            };
-            class rwrEnemy: rwr
-            {
-                color[]	= common_red;
-            };
-            //ground
-            class targetGround
-            {
-                class TargetBox { };
-            };
-            class targetAir
-            {
             };
         };
+        class markingThreat
+        {
+            color[] = {1,1,0}; // radar is tracking you actively
+            class background {
+            	type        = "polygon";
+            	points[] ={
+            		{
+            			{{-0.015, 0.015},1},
+            			{{0.015, 0.015},1},
+            			{{0.015, -0.015},1},
+            			{{-0.015, -0.015},1}
+            		}
+            	}; // points
+            }; // background
+            class unknownTarget
+            {
+                type = "line";
+                width = 5.0000;
+                points[] ={
+                    {{-0.015, -0.015},1},
+                    {{0.015, -0.015},1},
+                    {{0.015, 0.015},1},
+                    {{-0.015, 0.015},1},
+                    {{-0.015, -0.015},1},{},
+                    {{-0.002, -0.002},1},
+                    {{0.002, 0.002},1},{},
+                    {{-0.002, 0.002},1},
+                    {{0.002, -0.002},1}
+                };
+            };
+        };
+        class lockingThreat
+        {
+            color[] = {1,0,0};
+            class background {
+            	type        = "polygon";
+            	points[] ={
+            		{
+            			{{-0.015, 0.015},1},
+            			{{0.015, 0.015},1},
+            			{{0.015, -0.015},1},
+            			{{-0.015, -0.015},1}
+            		}
+            	}; // points
+            }; // background
+            class TargetBox
+            {
+                type = "line"; // locking, you're in for a bad time
+                width = 5.0000;
+                points[] ={
+                    {{-0.015,0},1},
+                    {{0, -0.015 },1},
+                    {{0.015,0 },1},
+                    {{0, 0.015 },1},
+                    {{-0.015,0},1},{},
+                    {{-0.005,0.005},1},
+                    {{0,-0.005},1},
+                    {{0.005,0.005},1}
+                };
+            };
+        };
+        //rwr
+        class rwr
+        {
+            class TargetBox
+            {
+                type = "line"; // there's a radar, but stuff isn't happening yet
+                width = 5.0000;
+                points[] ={
+                    {{-0.015, -0.015},1},
+                    {{0.015, -0.015},1},
+                    {{0.015, 0.015},1},
+                    {{-0.015, 0.015},1},
+                    {{-0.015, -0.015},1},{},
+                    {{-0.005,0.005},1},
+                    {{0,-0.005},1},
+                    {{0.005,0.005},1}
+                };
+            };
+        };
+        class rwrFriendly: rwr
+        {
+           color[]	= common_green;
+        };
+        class rwrEnemy: rwr
+        {
+          color[]	= common_red;
+        };
+        //ground
+        class targetGround
+        {
+            class TargetBox { };
+        };
+        class targetAir
+        {
+        };
+    };
 };
