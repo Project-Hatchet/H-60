29676750fa67d71314823902619fdfd291c26306
Steve Zhao <ampersand38@gmail.com>
2021-11-25 23:29:24 -0500

    spaces

--- addons/uh60_mfd/config/interaction.hpp
+++ addons/uh60_mfd/config/interaction.hpp
@@ -28,19 +28,19 @@ class mfd_any {
 
 class tac {
     condition= USERVAL(MFD_PAGE_INDEX,MFD_PAGE_TAC);
-    MFD_BTN(MFD_20,QUOTE(Zoom out)) buttonUp="[vehicle player,'zoom', -1] call vtx_uh60_mfd_fnc_interaction_tac;"; };
-    MFD_BTN(MFD_21,QUOTE(Zoom in)) buttonUp="[vehicle player,'zoom', 1] call vtx_uh60_mfd_fnc_interaction_tac;"; };
+    MFD_BTN(MFD_20,QUOTE(Zoom out)) buttonUp="[vxf_vehicle,'zoom', -1] call vtx_uh60_mfd_fnc_interaction_tac;"; };
+    MFD_BTN(MFD_21,QUOTE(Zoom in)) buttonUp="[vxf_vehicle,'zoom', 1] call vtx_uh60_mfd_fnc_interaction_tac;"; };
 };
 
 class jvmf {
     condition= USERVAL(MFD_PAGE_INDEX,MFD_PAGE_JVMF);
-    MFD_BTN(MFD_23,QUOTE(Prev MSG)) buttonUp="[vehicle player,'cycle', -1] call vtx_uh60_jvmf_fnc_mfdInteraction;"; };
-    MFD_BTN(MFD_22,QUOTE(Next MSG)) buttonUp="[vehicle player,'cycle', 1] call vtx_uh60_jvmf_fnc_mfdInteraction;"; };
+    MFD_BTN(MFD_23,QUOTE(Prev MSG)) buttonUp="[vxf_vehicle,'cycle', -1] call vtx_uh60_jvmf_fnc_mfdInteraction;"; };
+    MFD_BTN(MFD_22,QUOTE(Next MSG)) buttonUp="[vxf_vehicle,'cycle', 1] call vtx_uh60_jvmf_fnc_mfdInteraction;"; };
     MFD_BTN(MFD_8,QUOTE(New message)) buttonUp="createDialog 'vtx_uh60_jvmf_writeDialog';"; };
-    MFD_BTN(MFD_1,QUOTE(ACK)) buttonUp="[vehicle player, 'ACK'] call vtx_uh60_jvmf_fnc_reply"; };
-    MFD_BTN(MFD_2,QUOTE(WILCO)) buttonUp="[vehicle player, 'WILCO'] call vtx_uh60_jvmf_fnc_reply"; };
-    MFD_BTN(MFD_5,QUOTE(HAVECO)) buttonUp="[vehicle player, 'HAVECO'] call vtx_uh60_jvmf_fnc_reply"; };
-    MFD_BTN(MFD_6,QUOTE(CANTCO)) buttonUp="[vehicle player, 'CANTCO'] call vtx_uh60_jvmf_fnc_reply"; };
+    MFD_BTN(MFD_1,QUOTE(ACK)) buttonUp="[vxf_vehicle, 'ACK'] call vtx_uh60_jvmf_fnc_reply"; };
+    MFD_BTN(MFD_2,QUOTE(WILCO)) buttonUp="[vxf_vehicle, 'WILCO'] call vtx_uh60_jvmf_fnc_reply"; };
+    MFD_BTN(MFD_5,QUOTE(HAVECO)) buttonUp="[vxf_vehicle, 'HAVECO'] call vtx_uh60_jvmf_fnc_reply"; };
+    MFD_BTN(MFD_6,QUOTE(CANTCO)) buttonUp="[vxf_vehicle, 'CANTCO'] call vtx_uh60_jvmf_fnc_reply"; };
 };
 
 class flir {
