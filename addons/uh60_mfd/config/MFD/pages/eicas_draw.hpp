09389a98419da616ac7afc720ae3520715285dde
BradMick <bradMick@example.com>
2022-01-06 11:40:15 -0600

    Update
    Quick fix to align starter dropout with existing SFM+ values

--- addons/uh60_mfd/config/MFD/pages/eicas_draw.hpp
+++ addons/uh60_mfd/config/MFD/pages/eicas_draw.hpp
@@ -211,7 +211,7 @@ class blackTextWrapper {
 };
 
 class eng1_out {
-	condition = "user19<40";
+	condition = "user19<52";
 	color[] = {1,0,0,1};
     class cross {
     	type="line";
@@ -225,7 +225,7 @@ class eng1_out {
     }; // Cross
 };
 class eng2_out {
-	condition = "user45<40";
+	condition = "user45<52";
 	color[] = {1,0,0,1};
     class cross {
     	type="line";
