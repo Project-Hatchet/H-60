df8bb199fec2e4d3ccbbc7a5323b8fa008bea425
Warren <rivera.john02@gmail.com>
2024-12-08 21:42:54 +0300

    Update Tracers for Minigun

--- addons/UH60/config/cfgMagazines.hpp
+++ addons/UH60/config/cfgMagazines.hpp
@@ -2,5 +2,6 @@ class cfgMagazines {
     class 2000Rnd_65x39_Belt_Tracer_Red;
     class vtx_2000Rnd_65x39_Belt_Tracer_Red: 2000Rnd_65x39_Belt_Tracer_Red {
         count = 4400;
+        tracersEvery = 5;
     };
 };
