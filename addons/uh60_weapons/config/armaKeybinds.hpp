5d3280df7ac65a7913a27e3d408572cda940a5e0
BroBeans <cpgbrobeans@gmail.com>
2025-05-06 11:05:32 +0800

    changes

--- addons/uh60_weapons/config/armaKeybinds.hpp
+++ addons/uh60_weapons/config/armaKeybinds.hpp
@@ -8,37 +8,31 @@ class CfgUserActions
 		onAnalog = "";	// _this is the scalar analog value.
 		analogChangeThreshold = 0.1; // Minimum change required to trigger the onAnalog EH (default: 0.01).
 	};
-	class vtx_weapons_fireHellfireK : vtx_weapons_fireLaser {
-		displayName = "Fire 114K";
+	class vtx_weapons_fireRocket : vtx_weapons_fireLaser {
+		displayName = "Fire Rockets";
 		tooltip = "Hit this key to fire mentioned weapon.";
-		onActivate = "[vehicle player, 'vtx_hellfire_launcher', 'vtx_hellfire_launcher'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";		// _this is always true.
-	};
-	class vtx_weapons_fireHellfireN : vtx_weapons_fireLaser {
-		displayName = "Fire 114N";
-		tooltip = "Hit this key to fire mentioned weapon.";
-		onActivate = "[vehicle player, 'vtx_hellfire_launcher_N', 'vtx_hellfire_launcher_N'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";		// _this is always true.
+		onActivate = "[vehicle player, 'vtx_hydra', 'single'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";		
 	};
 	class vtx_weapons_fireAPKWS : vtx_weapons_fireLaser {
 		displayName = "Fire APKWS";
 		tooltip = "Hit this key to fire mentioned weapon.";
-		onActivate = "[vehicle player, 'vtx_apkws', 'Medium_AI'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";		// _this is always true.
+		onActivate = "[vehicle player, 'vtx_apkws', 'Medium_AI'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";	
 	};
 	class vtx_weapons_fireMissile : vtx_weapons_fireLaser {
 		displayName = "Fire Missile";
 		tooltip = "Hit this button to fire a missile, priority - 114K, 114N, APKWS.";
-		onActivate = "[vehicle player, 'MISSILE'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";		// _this is always true.
+		onActivate = "[vehicle player, 'MISSILE'] call vtx_uh60_weapons_fnc_fireAndResetWeapon;";	
 	};
 };
 class CfgDefaultKeysPresets {};
 class UserActionGroups
 {
-	class Vtx_KeyBinds_FLIR {
+	class vtx_keybinds_weapons {
 		name = "H-60 Weapons"; // Display name of your category.
 		isAddon = 1;
 		group[] = {
 		"vtx_weapons_fireAPKWS",
-		"vtx_weapons_fireHellfireK",
-		"vtx_weapons_fireHellfireN",
+		"vtx_weapons_fireRocket",
 		"vtx_weapons_fireLaser",
 		"vtx_weapons_fireMissile"
 		}; // List of all actions inside this category.
