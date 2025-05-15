fd9adcc6b61e8f321275319f86daaec61f98c119
Steve Zhao <ampersand38@gmail.com>
2025-05-15 00:36:20 -0400

    FMS shows selected weapon

--- addons/uh60_fms/config/MFD/fms/wpn_draw.hpp
+++ addons/uh60_fms/config/MFD/fms/wpn_draw.hpp
@@ -3,72 +3,174 @@ TEXT_FMS_C(HEADER_TEXT,0.5,FMS_YHEAD)
 	text = "WEAPON SYSTEMS";
 }; // HEADER_TEXT
 
-TEXT_FMS_C(STORES,0.5,FMS_Y1)
-	source = "static";
-	text = "STORES";
-}; // STA1_LBL
-TEXT_FMS_C(STA1_LBL,0.3,FMS_Y12)
+class STORES {
+    source = "static";
+    text = "";};
+
+#define GROUP_STATION_SELECTED_BLINK type = "group"; \
+  blinkingPattern[] = {0.2, 0.2}; \
+  blinkingStartsOn = 1; \
+  color[] = {0, 0, 0, 1}
+
+TEXT_FMS_L(STA1_LBL,FMS_MARGIN_L,FMS_Y1)
 	source = "pylonMagazineNameShort";
   sourceIndex = 1;
   sourceScale = 1;
-	//text = "STA1";
 }; // STA1_LBL
-TEXT_FMS_C(STA2_LBL,0.7,FMS_Y12)
-	source = "pylonMagazineNameShort";
-  sourceIndex = 2;
-  sourceScale = 1;
-	//text = "STA2";
-}; // STA2_LBL
-
-TEXT_FMS_C(STA1_AMMO,0.3,FMS_Y2)
+TEXT_FMS_L(STA1_AMMO,FMS_MARGIN_L,FMS_Y12)
 	source = "pylonAmmo";
   sourceIndex = 1;
   sourceScale = 1;
-  text = "";
 }; // STA1_AMMO
-TEXT_FMS_C(STA2_AMMO,0.7,FMS_Y2)
+class Sta1_sel {
+  condition = "pylonSelected1>0";
+  GROUP_STATION_SELECTED_BLINK;
+
+  TEXT_FMS_L(STA1_LBL,FMS_MARGIN_L,FMS_Y1)
+    source = "pylonMagazineNameShort";
+    sourceIndex = 1;
+    sourceScale = 1;
+  }; // STA1_LBL
+  TEXT_FMS_L(STA1_AMMO,FMS_MARGIN_L,FMS_Y12)
+    source = "pylonAmmo";
+    sourceIndex = 1;
+    sourceScale = 1;
+  }; // STA1_AMMO
+};
+
+TEXT_FMS_R(STA2_LBL,FMS_MARGIN_R,FMS_Y1)
+	source = "pylonMagazineNameShort";
+  sourceIndex = 2;
+  sourceScale = 1;
+}; // STA2_LBL
+TEXT_FMS_R(STA2_AMMO,FMS_MARGIN_R,FMS_Y12)
 	source = "pylonAmmo";
   sourceIndex = 2;
   sourceScale = 1;
-  text = "";
 }; // STA2_AMMO
+class Sta2_sel {
+  condition = "pylonSelected2>0";
+  GROUP_STATION_SELECTED_BLINK;
+  TEXT_FMS_R(STA2_LBL,FMS_MARGIN_R,FMS_Y1)
+    source = "pylonMagazineNameShort";
+    sourceIndex = 2;
+    sourceScale = 1;
+  }; // STA2_LBL
+  TEXT_FMS_R(STA2_AMMO,FMS_MARGIN_R,FMS_Y12)
+    source = "pylonAmmo";
+    sourceIndex = 2;
+    sourceScale = 1;
+  }; // STA2_AMMO
+};
 
-TEXT_FMS_R(CM_LBL,0.5,FMS_Y22)
+#define SOURCEINDEX 48
+TEXT_FMS_L(STA3_LBL,FMS_MARGIN_L,FMS_Y2)
+	source = "pylonMagazineNameShort";
+  sourceIndex = SOURCEINDEX;
+  sourceScale = 1;
+}; // STA3_LBL
+TEXT_FMS_L(STA3_AMMO,FMS_MARGIN_L,FMS_Y22)
+	source = "pylonAmmo";
+  sourceIndex = SOURCEINDEX;
+  sourceScale = 1;
+}; // STA3_AMMO
+class Sta3_sel {
+  condition = "pylonSelected51>0";
+  GROUP_STATION_SELECTED_BLINK;
+  TEXT_FMS_L(STA3_LBL,FMS_MARGIN_L,FMS_Y2)
+    source = "pylonMagazineNameShort";
+    sourceIndex = SOURCEINDEX;
+    sourceScale = 1;
+  }; // STA3_LBL
+  TEXT_FMS_L(STA3_AMMO,FMS_MARGIN_L,FMS_Y22)
+    source = "pylonAmmo";
+    sourceIndex = SOURCEINDEX;
+    sourceScale = 1;
+  }; // STA3_AMMO
+};
+
+#define SOURCEINDEX 49
+TEXT_FMS_R(STA4_LBL,FMS_MARGIN_R,FMS_Y2)
+	source = "pylonMagazineNameShort";
+  sourceIndex = SOURCEINDEX;
+  sourceScale = 1;
+}; // STA4_LBL
+TEXT_FMS_R(STA4_AMMO,FMS_MARGIN_R,FMS_Y22)
+	source = "pylonAmmo";
+  sourceIndex = SOURCEINDEX;
+  sourceScale = 1;
+}; // STA4_AMMO
+class Sta4_sel {
+  condition = "pylonSelected50>0";
+  GROUP_STATION_SELECTED_BLINK;
+  TEXT_FMS_R(STA4_LBL,FMS_MARGIN_R,FMS_Y2)
+    source = "pylonMagazineNameShort";
+    sourceIndex = SOURCEINDEX;
+    sourceScale = 1;
+  }; // STA4_LBL
+  TEXT_FMS_R(STA4_AMMO,FMS_MARGIN_R,FMS_Y22)
+    source = "pylonAmmo";
+    sourceIndex = SOURCEINDEX;
+    sourceScale = 1;
+  }; // STA4_AMMO
+};
+
+#define FMS_X_AMMO 0.6
+TEXT_FMS_R(CM_LBL,0.5,FMS_Y32)
 	source = "cmWeapon";
   sourceIndex = 0;
   sourceScale = 1;
   text = "";
-}; // STA2_LBL
-TEXT_FMS_L(CM_AMMO,0.55,FMS_Y22)
+}; // CMW1_LBL
+TEXT_FMS_L(CM_AMMO,FMS_X_AMMO,FMS_Y32)
 	source = "cmAmmo";
   sourceIndex = 0;
   sourceScale = 1;
   text = "";
-}; // STA2_AMMO
-TEXT_FMS_R(CM_2_LBL,0.5,FMS_Y3)
+}; // CMW1_AMMO
+
+TEXT_FMS_R(CM_2_LBL,0.5,FMS_Y4)
 	source = "cmWeapon";
   sourceIndex = 1;
   sourceScale = 1;
   text = "";
-}; // STA2_LBL
-TEXT_FMS_L(CM_2_AMMO,0.55,FMS_Y3)
+}; // CMW2_LBL
+TEXT_FMS_L(CM_2_AMMO,FMS_X_AMMO,FMS_Y4)
 	source = "cmAmmo";
   sourceIndex = 1;
   sourceScale = 1;
   text = "";
-}; // STA2_AMMO
-TEXT_FMS_R(CM_3_LBL,0.5,FMS_Y32)
+}; // CMW2_AMMO
+class CMW2_sel {condition = "0";};
+
+TEXT_FMS_R(CM_3_LBL,0.5,FMS_Y42)
 	source = "cmWeapon";
   sourceIndex = 2;
   sourceScale = 1;
   text = "";
-}; // STA2_LBL
-TEXT_FMS_L(CM_3_AMMO,0.55,FMS_Y32)
+}; // CMW3_LBL
+TEXT_FMS_L(CM_3_AMMO,FMS_X_AMMO,FMS_Y42)
 	source = "cmAmmo";
   sourceIndex = 2;
   sourceScale = 1;
   text = "";
-}; // STA2_AMMO
+}; // CMW3_AMMO
+class CMW3_sel {
+  condition = "mgun * (1 - pylonSelected1) * (1 - pylonSelected2)";
+  GROUP_STATION_SELECTED_BLINK;
+  TEXT_FMS_R(CM_3_LBL,0.5,FMS_Y42)
+    source = "cmWeapon";
+    sourceIndex = 2;
+    sourceScale = 1;
+    text = "";
+  }; // CMW3_LBL
+  TEXT_FMS_L(CM_3_AMMO,FMS_X_AMMO,FMS_Y42)
+    source = "cmAmmo";
+    sourceIndex = 2;
+    sourceScale = 1;
+    text = "";
+  }; // CMW3_AMMO
+};
 
 TEXT_FMS_L(BACK,FMS_MARGIN_L,FMS_Y5)
 	source = "static";
