b3bbd0da008f8c96c7352765c29c9915e9bf7d3b
Steve Zhao <ampersand38@gmail.com>
2025-07-15 17:20:52 -0400

    remove pylons from models without wing stores

--- addons/UH60/config/cfgVehiclesParts/components.hpp
+++ addons/UH60/config/cfgVehiclesParts/components.hpp
@@ -1,27 +1,4 @@
 class Components: Components {
-	class TransportPylonsComponent {
-    	uiPicture = "z\vtx\addons\MH60M\Data\UI\vtx_mh60m_dap_3den_ca.paa";
-		class Pylons {
-			class pylons1 {
-				attachment = "";
-				priority = 5;
-				hardpoints[] = {};
-				UIposition[] = {0.59, 0.41};
-			};
-			class pylons2: pylons1 {
-				attachment = "";
-				mirroredMissilePos = 1;
-				hardpoints[] = {};
-				UIposition[] = {0.08, 0.41};
-			};
-		};
-		class Presets {
-			class Empty {
-				displayName = "Empty";
-				attachment[] = {};
-			};
-		};
-	}; // TransportPylonsComponent
 	class SensorsManagerComponent {
 		class Components {
 			class LaserSensorComponent: SensorTemplateLaser {};
